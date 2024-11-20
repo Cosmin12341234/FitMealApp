869db08a8c0db28b8720d78e5a3278e28aebb559
Cosmin12341234 <neanu.cosmin@gmail.com>
2024-11-20 15:50:13 +0200

    SignIn and SignUp Pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import { Toaster } from "sonner";
-import SignInForm from "@/components/SignInForm.tsx";
-const App = () => {
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'sonner';
+import SignUpPage from "@/pages/auth/SignUpPage.tsx";
+import SignInPage from "@/pages/auth/SignInPage.tsx";
+
+function App() {
     return (
-        <>
-            <SignInForm />
-            <Toaster position="top-right" richColors />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/signin" element={<SignInPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="*" element={<Navigate to="/signin" replace />} />
+            </Routes>
+            <Toaster richColors position="top-center" />
+        </BrowserRouter>
     );
-};
+}
 
 export default App;
---
+++ frontend/src/pages/auth/SignUpPage.tsx
@@ -0,0 +1,13 @@
+import SignUpForm from "@/components/SignUpForm";
+
+const SignUpPage = () => {
+    return (
+        <div className="min-h-screen bg-[#E9DDD4]">
+            <div className="container mx-auto px-4 py-8">
+                <SignUpForm />
+            </div>
+        </div>
+    );
+};
+
+export default SignUpPage;
