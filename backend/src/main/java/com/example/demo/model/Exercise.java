78a488822736d0157d5fc5a59e1fd2fac6ad8424
Cosmin12341234 <neanu.cosmin@gmail.com>
2024-12-03 19:37:32 +0200

    Exercise Entity for admin and workout generator

---
+++ backend/src/main/java/com/example/demo/model/Exercise.java
@@ -0,0 +1,63 @@
+package com.example.demo.model;
+
+import com.example.demo.model.enums.Difficulty;
+import com.example.demo.model.enums.Equipment;
+import com.example.demo.model.enums.Muscle;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "app_exercise")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "The id of the exercise")
+    private Long id;
+
+    @Column(nullable = false, length = 64)
+    @Schema(description = "The name of the exercise")
+    private String name;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Schema(description = "The primary muscle of the exercise")
+    private Muscle primaryMuscle;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @ElementCollection
+    @Schema(description = "The secondary muscles of the exercise")
+    private List<Muscle> secondaryMuscles;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Schema(description = "The equipment of the exercise")
+    private Equipment equipment;
+
+    @Column(nullable = false)
+    @Schema(description = "The difficulty of the exercise")
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
+
+    @Column(nullable = false, length = 256)
+    @Schema(description = "The instructions of the exercise")
+    private String instructions;
+
+    public Exercise(String name, Muscle primaryMuscle, List<Muscle> secondaryMuscles, Equipment equipment, Difficulty difficulty, String instructions) {
+        this.name = name;
+        this.primaryMuscle = primaryMuscle;
+        this.secondaryMuscles = secondaryMuscles;
+        this.equipment = equipment;
+        this.difficulty = difficulty;
+        this.instructions = instructions;
+    }
+}
---
+++ backend/src/main/java/com/example/demo/model/enums/Equipment.java
@@ -0,0 +1,21 @@
+package com.example.demo.model.enums;
+
+public enum Equipment {
+    BARBELL,
+    DUMBBELLS,
+    BODYWEIGHT,
+    MACHINE,
+    MEDICINE_BALL,
+    KETTLEBELLS,
+    STRETCHES,
+    CABLES,
+    BAND,
+    PLATE,
+    TRX,
+    YOGA,
+    BOSU_BALL,
+    VITRUVIAN,
+    CARDIO,
+    SMITH_MACHINE,
+    RECOVERY;
+}
